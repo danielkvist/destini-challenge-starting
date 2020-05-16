3526939bbd491c485dbe096e2b0df3c021fe7fd3
Danielkvist <danielkvist@protonmail.com>
2020-05-16 11:24:48 +0200

    add getters to StoryBrain

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'story_brain.dart';
 
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
-
 void main() => runApp(Destini());
 
 class Destini extends StatelessWidget {
@@ -59,8 +57,7 @@ class _StoryPageState extends State<StoryPage> {
                   },
                   color: Colors.red,
                   child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    'Choice 1',
+                    storyBrain.choice1,
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
@@ -81,8 +78,7 @@ class _StoryPageState extends State<StoryPage> {
                   },
                   color: Colors.blue,
                   child: Text(
-                    //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                    'Choice 2',
+                    storyBrain.choice2,
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
